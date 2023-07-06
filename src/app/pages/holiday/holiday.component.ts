243b00e38f056d6ddd9e41f6ecc6eeb4438e0aed
BerkayCan Ozcelik <brkycn10@gmail.com>
2023-07-06 16:29:57 +0200

    added holidayForm and holidayTable to holidayPage

--- src/app/pages/holiday/holiday.component.ts
+++ src/app/pages/holiday/holiday.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MatTabsModule } from '@angular/material/tabs';
+import { HolidayFormComponent } from 'src/app/components/holiday-form/holiday-form.component';
+import { HolidayTableComponent } from "../../components/holiday-table/holiday-table.component";
 
 @Component({
-  selector: 'app-holiday',
-  standalone: true,
-  imports: [CommonModule],
-  templateUrl: './holiday.component.html',
-  styleUrls: ['./holiday.component.scss']
+    selector: 'app-holiday',
+    standalone: true,
+    templateUrl: './holiday.component.html',
+    styleUrls: ['./holiday.component.scss'],
+    imports: [CommonModule, MatTabsModule, HolidayFormComponent, HolidayTableComponent]
 })
-export class HolidayComponent {
-
-}
+export class HolidayComponent {}
