f90d50b41261af99def8120ca8c180b6ee89c56a
BerkayCan Ozcelik <brkycn10@gmail.com>
2023-07-06 15:37:00 +0200

    imported pages and components to app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NavbarComponent } from "./components/shared/navbar/navbar.component";
+import { FooterComponent } from "./components/shared/footer/footer.component";
 
 @NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule],
-  providers: [],
-  bootstrap: [AppComponent],
+    declarations: [AppComponent],
+    providers: [],
+    bootstrap: [AppComponent],
+    imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule, NavbarComponent, FooterComponent]
 })
 export class AppModule {}
