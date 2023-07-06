723fa0c6ae281314f84c1ab475839df47708c280
BerkayCan Ozcelik <brkycn10@gmail.com>
2023-07-06 15:37:23 +0200

    added routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './pages/login/login.component';
+import { LandingComponent } from './pages/landing/landing.component';
+import { ProfileComponent } from './pages/profile/profile.component';
+import { HolidayComponent } from './pages/holiday/holiday.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/login', pathMatch: 'full' }, // Default route
+  { path: 'login', component: LoginComponent },
+  { path: 'landing', component: LandingComponent },
+  { path: 'profile', component: ProfileComponent },
+  { path: 'holiday', component: HolidayComponent },
+  { path: '**', redirectTo: '/login' }, // Redirect to login page for any other route
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
