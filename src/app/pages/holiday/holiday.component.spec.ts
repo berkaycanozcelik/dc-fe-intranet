d185b82767c4c03e86a4f11fdc373fbbbb351f03
BerkayCan Ozcelik <brkycn10@gmail.com>
2023-07-06 15:12:35 +0200

    created holiday page

---
+++ src/app/pages/holiday/holiday.component.spec.ts
@@ -0,0 +1,21 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { HolidayComponent } from './holiday.component';
+
+describe('HolidayComponent', () => {
+  let component: HolidayComponent;
+  let fixture: ComponentFixture<HolidayComponent>;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [HolidayComponent]
+    });
+    fixture = TestBed.createComponent(HolidayComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/pages/holiday/holiday.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-holiday',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './holiday.component.html',
+  styleUrls: ['./holiday.component.scss']
+})
+export class HolidayComponent {
+
+}
