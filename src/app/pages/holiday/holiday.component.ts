7c4d61098bc5dbabd0f8208566fc02a1adee1173
BerkayCan Ozcelik <brkycn10@gmail.com>
2023-07-07 13:15:40 +0200

    adjusted holiday page

--- src/app/pages/holiday/holiday.component.ts
+++ src/app/pages/holiday/holiday.component.ts
@@ -2,13 +2,26 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatTabsModule } from '@angular/material/tabs';
 import { HolidayFormComponent } from 'src/app/components/holiday-form/holiday-form.component';
-import { HolidayTableComponent } from "../../components/holiday-table/holiday-table.component";
+import HolidayTableComponent from '../../components/holiday-table/holiday-table.component';
+import { Holiday } from '../../types/holiday';
 
 @Component({
-    selector: 'app-holiday',
-    standalone: true,
-    templateUrl: './holiday.component.html',
-    styleUrls: ['./holiday.component.scss'],
-    imports: [CommonModule, MatTabsModule, HolidayFormComponent, HolidayTableComponent]
+  selector: 'app-holiday',
+  standalone: true,
+  templateUrl: './holiday.component.html',
+  styleUrls: ['./holiday.component.scss'],
+  imports: [
+    CommonModule,
+    MatTabsModule,
+    HolidayFormComponent,
+    HolidayTableComponent,
+  ],
 })
-export class HolidayComponent {}
+export class HolidayComponent {
+  holidays: Holiday[] = [];
+
+  onHolidaysChange(holidays: Holiday[]) {
+    this.holidays = holidays;
+    console.log('Form in Holiday Comp. Holidays:', this.holidays);
+  }
+}
