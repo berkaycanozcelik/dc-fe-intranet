975f97c05b9d74a03a03dfe245e06634399d74fc
BerkayCan Ozcelik <brkycn10@gmail.com>
2023-07-07 13:15:20 +0200

    adjsuted holiday table

--- src/app/components/holiday-table/holiday-table.component.spec.ts
+++ src/app/components/holiday-table/holiday-table.component.spec.ts
@@ -1,6 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { HolidayTableComponent } from './holiday-table.component';
+import HolidayTableComponent from './holiday-table.component';
 
 describe('HolidayTableComponent', () => {
   let component: HolidayTableComponent;
@@ -8,7 +8,7 @@ describe('HolidayTableComponent', () => {
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      imports: [HolidayTableComponent]
+      imports: [HolidayTableComponent],
     });
     fixture = TestBed.createComponent(HolidayTableComponent);
     component = fixture.componentInstance;
--- src/app/components/holiday-table/holiday-table.component.ts
+++ src/app/components/holiday-table/holiday-table.component.ts
@@ -1,7 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatTableModule } from '@angular/material/table';
 import { MatChipsModule } from '@angular/material/chips';
+import { Holiday } from '../../types/holiday';
+import { MatIconModule } from '@angular/material/icon';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatButtonModule } from '@angular/material/button';
 
 export interface PeriodicElement {
   name: string;
@@ -10,20 +14,22 @@ export interface PeriodicElement {
   symbol: string;
 }
 
-const ELEMENT_DATA: PeriodicElement[] = [
-  { position: 1, name: 'Pfingsten', weight: 3, symbol: 'Accepted' },
-  { position: 2, name: 'Silvester', weight: 2, symbol: 'Accepted' },
-  { position: 3, name: 'Ostern', weight: 6, symbol: 'Accepted' },
-];
-
 @Component({
   selector: 'app-holiday-table',
   standalone: true,
-  imports: [CommonModule, MatTableModule, MatChipsModule],
+  imports: [
+    CommonModule,
+    MatTableModule,
+    MatChipsModule,
+    MatButtonModule,
+    MatDividerModule,
+    MatIconModule,
+  ],
   templateUrl: './holiday-table.component.html',
   styleUrls: ['./holiday-table.component.scss'],
 })
-export class HolidayTableComponent {
-  displayedColumns: string[] = ['position', 'name', 'weight', 'symbol'];
-  dataSource = ELEMENT_DATA;
+export default class HolidayTableComponent {
+  @Input() holidays: Holiday[] = [];
+
+  dataSource = this.holidays;
 }
