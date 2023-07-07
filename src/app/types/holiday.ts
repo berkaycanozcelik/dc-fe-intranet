acb9d96033c07d50d0815ed37953f38364e6fa63
BerkayCan Ozcelik <brkycn10@gmail.com>
2023-07-07 13:14:45 +0200

    created holiday type

---
+++ src/app/types/holiday.ts
@@ -0,0 +1,11 @@
+export type Holiday = {
+    startDate: string;
+    endDate: string;
+    remainingDays: number;
+    vacationWorkdays: number;
+    reason: string;
+    confirmation1: boolean;
+    confirmation2: boolean;
+    replacement: string;
+    status: string;
+  }
