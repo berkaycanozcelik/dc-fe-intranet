e136cd5e2c26959ee2a6f44cff9a00ff79fa311a
BerkayCan Ozcelik <brkycn10@gmail.com>
2023-07-10 17:36:11 +0200

    added delete functionality

--- src/app/components/holiday-table/holiday-table.component.ts
+++ src/app/components/holiday-table/holiday-table.component.ts
@@ -44,4 +44,24 @@ export default class HolidayTableComponent {
     this.editHoliday.emit(this.holidays[i]);
     this.router.navigate(['/holiday']);
   }
+
+  onDelete(i: number): void {
+    let deleteHoliday = this.holidays[i];
+    this.holidayService.deleteHoliday(deleteHoliday.id!).subscribe(
+      () => {
+        // Find the index of the holiday to delete
+        const index = this.holidays.findIndex(
+          (h) => h.id === deleteHoliday.id!
+        );
+        if (index !== -1) {
+          // Splice the holiday from the array
+          this.holidays.splice(index, 1);
+          console.log('Holiday deleted successfully:', deleteHoliday.id!);
+        }
+      },
+      (error) => {
+        console.error('Error deleting holiday:', error);
+      }
+    );
+  }
 }
