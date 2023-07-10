8621db5355c6309635a494ed0d0cb2f9e3312712
BerkayCan Ozcelik <brkycn10@gmail.com>
2023-07-10 11:21:43 +0200

    addded import for httpRequests

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { NavbarComponent } from "./components/shared/navbar/navbar.component";
-import { FooterComponent } from "./components/shared/footer/footer.component";
+import { NavbarComponent } from './components/shared/navbar/navbar.component';
+import { FooterComponent } from './components/shared/footer/footer.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
-    declarations: [AppComponent],
-    providers: [],
-    bootstrap: [AppComponent],
-    imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule, NavbarComponent, FooterComponent]
+  declarations: [AppComponent],
+  providers: [],
+  bootstrap: [AppComponent],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    NavbarComponent,
+    FooterComponent,
+    HttpClientModule,
+  ],
 })
 export class AppModule {}
