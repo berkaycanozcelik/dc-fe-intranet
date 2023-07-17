90a689b1412f3502a73707903de52a864936afe6
BerkayCan Ozcelik <brkycn10@gmail.com>
2023-07-17 17:37:49 +0200

    adjusted LoginForm

--- src/app/components/login-form/login-form.component.ts
+++ src/app/components/login-form/login-form.component.ts
@@ -1,13 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { LoginService } from 'src/app/services/login/login.service';
+import { LoginData } from 'src/app/models/loginData';
+import {
+  FormControl,
+  FormGroup,
+  FormsModule,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-login-form',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule],
   templateUrl: './login-form.component.html',
-  styleUrls: ['./login-form.component.scss']
+  styleUrls: ['./login-form.component.scss'],
 })
 export class LoginFormComponent {
+  email: string = '';
+  password: string = '';
 
+  constructor(private loginService: LoginService) {}
+
+  submitForm() {
+    this.loginService.login(this.email, this.password).subscribe(
+      (response) => {
+        console.log(response);
+      },
+      (error) => {
+        console.log(error);
+      }
+    );
+  }
 }
