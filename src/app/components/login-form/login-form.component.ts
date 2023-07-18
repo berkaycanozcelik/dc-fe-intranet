48013dac3e37d9f5945293942a35cd1bbd369e5d
BerkayCan Ozcelik <brkycn10@gmail.com>
2023-07-18 14:09:01 +0200

    improved login form

--- src/app/components/login-form/login-form.component.ts
+++ src/app/components/login-form/login-form.component.ts
@@ -1,13 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LoginService } from 'src/app/services/login/login.service';
-import { LoginData } from 'src/app/models/loginData';
-import {
-  FormControl,
-  FormGroup,
-  FormsModule,
-  Validators,
-} from '@angular/forms';
+import { FormsModule, NgForm } from '@angular/forms';
 import { Router } from '@angular/router';
 
 @Component({
@@ -18,15 +12,14 @@ import { Router } from '@angular/router';
   styleUrls: ['./login-form.component.scss'],
 })
 export class LoginFormComponent {
-  email: string = '';
-  password: string = '';
-
   isLoggedIn: boolean = false;
 
   constructor(private loginService: LoginService, private router: Router) {}
 
-  submitForm() {
-    this.loginService.login(this.email, this.password).subscribe(
+  onSubmit(form: NgForm) {
+    console.log(form.value);
+
+    this.loginService.login(form.value.email, form.value.password).subscribe(
       (response) => {
         sessionStorage.setItem('token', response.token);
         this.isLoggedIn = true;
