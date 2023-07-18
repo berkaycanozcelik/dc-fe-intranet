4e6320cd89c5b6f127a18f987d99acbd9ad9e2c6
BerkayCan Ozcelik <brkycn10@gmail.com>
2023-07-18 17:12:51 +0200

    created userModel

---
+++ src/app/models/user.model.ts
@@ -0,0 +1,17 @@
+import { Role } from './role';
+
+export class User {
+  constructor(
+    public id: string,
+    public role: Role,
+    private _token: string,
+    private _tokenExpirationDate: Date
+  ) {}
+
+  get token() {
+    if (!this._tokenExpirationDate || new Date() > this._tokenExpirationDate) {
+      return null;
+    }
+    return this._token;
+  }
+}
