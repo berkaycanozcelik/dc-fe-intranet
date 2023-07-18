12971cfe7103c5068999fc9291764690b1ba2666
BerkayCan Ozcelik <brkycn10@gmail.com>
2023-07-18 16:12:58 +0200

    added error handling

--- src/app/components/login-form/login-form.component.ts
+++ src/app/components/login-form/login-form.component.ts
@@ -1,8 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule, NgForm } from '@angular/forms';
 import { Router } from '@angular/router';
 import { AuthService } from 'src/app/services/auth/auth.service';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-login-form',
@@ -21,14 +22,14 @@ export class LoginFormComponent {
     this.isLoading = true;
     this.authService.login(form.value.email, form.value.password).subscribe(
       (response) => {
-        sessionStorage.setItem('token', response.token);
+        sessionStorage.setItem('token', JSON.stringify(response));
         this.isLoading = false;
         this.router.navigate(['/']);
         form.reset();
       },
-      (error) => {
+      (errorMessage) => {
         this.isLoading = false;
-        this.error = 'An error occured';
+        this.error = errorMessage;
       }
     );
   }
--- src/app/services/auth/auth.service.ts
+++ src/app/services/auth/auth.service.ts
@@ -1,13 +1,12 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Observable, catchError, throwError } from 'rxjs';
 import { LoginData } from 'src/app/models/loginData';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AuthService {
-
   constructor(private http: HttpClient) {}
 
   private baseUrl: string = 'http://localhost:8080/api/auth';
@@ -17,6 +16,35 @@ export class AuthService {
       email: email,
       password: password,
     };
-    return this.http.post(`${this.baseUrl}/authenticate`, loginData);
+    return (
+      this.http
+        .post(`${this.baseUrl}/authenticate`, loginData)
+
+        /* 
+    **
+     TODO: add more error handling after backend is ready
+    **
+    */
+        .pipe(
+          catchError((errorRes) => {
+            let errorMessage = 'An Error occurred';
+
+            if (!errorRes.status) {
+              errorMessage = "An unknown Error occurred"
+              return throwError(errorMessage);
+            }
+
+            switch (errorRes.status) {
+              case 403:
+                errorMessage = 'Authentication failed';
+                break;
+              case 404:
+                errorMessage = 'Server is offline';
+                break;
+            }
+            return throwError(errorMessage);
+          })
+        )
+    );
   }
 }
