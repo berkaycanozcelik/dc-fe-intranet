8fbc02d6f59ab5c345c03cae06fa52abfecb9c97
BerkayCan Ozcelik <brkycn10@gmail.com>
2023-07-19 11:09:30 +0200

    adjusted getHolidays call

--- src/app/services/holiday/holiday.service.ts
+++ src/app/services/holiday/holiday.service.ts
@@ -17,19 +17,7 @@ export class HolidayService {
   }
 
   getHolidays(): Observable<Holiday[]> {
-    //getting data as string
-    const jsonData = sessionStorage.getItem('data');
-
-    //converting to object
-    const objectData = jsonData !== null ? JSON.parse(jsonData) : null;
-    console.log('Token: ', objectData.token);
-
-    const headers = new HttpHeaders().set(
-      'Authorization',
-      `Bearer ${objectData.token}`
-    );
-
-    return this.http.get<Holiday[]>(this.baseUrl, { headers });
+    return this.http.get<Holiday[]>(this.baseUrl);
   }
 
   updateHoliday(holiday: Holiday, id: number): Observable<Holiday> {
