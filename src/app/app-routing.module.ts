0d1e710c0515f30d40164290e0f209b1ff95f809
BerkayCan Ozcelik <brkycn10@gmail.com>
2023-07-19 14:01:55 +0200

    added authGuard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,18 @@ import { LoginComponent } from './pages/login-page/login-page.component';
 import { LandingComponent } from './pages/landing-page/landing-page.component';
 import { ProfileComponent } from './pages/profile-page/profile-page.component';
 import { HolidayComponent } from './pages/holiday-page/holiday-page.component';
+import { AuthGuard } from './services/auth/auth.guard';
 
 const routes: Routes = [
-  { path: '', pathMatch: 'full', component: LandingComponent }, // Default route
+  {
+    path: '',
+    pathMatch: 'full',
+    component: LandingComponent,
+    canActivate: [AuthGuard],
+  }, // Default route
   { path: 'login', component: LoginComponent },
-  { path: 'profile', component: ProfileComponent },
-  { path: 'holiday', component: HolidayComponent },
+  { path: 'profile', component: ProfileComponent, canActivate: [AuthGuard] },
+  { path: 'holiday', component: HolidayComponent, canActivate: [AuthGuard] },
   { path: '**', redirectTo: '/' }, // Redirect to login page for any other route
 ];
 
