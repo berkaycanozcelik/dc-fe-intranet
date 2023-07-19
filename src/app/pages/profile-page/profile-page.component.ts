6c294c76f4c9b310bb32c726e8a1e8f38bea918b
BerkayCan Ozcelik <brkycn10@gmail.com>
2023-07-19 16:59:36 +0200

    getting userData via id

--- src/app/pages/profile-page/profile-page.component.ts
+++ src/app/pages/profile-page/profile-page.component.ts
@@ -1,8 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ProfileCardComponent } from 'src/app/components/profile/profile-card/profile-card.component';
 import { ProfileDetailCardComponent } from 'src/app/components/profile/profile-detail-card/profile-detail-card.component';
 import { RouterModule } from '@angular/router';
+import { UserService } from 'src/app/services/user/user.service';
+import { AuthService } from 'src/app/services/auth/auth.service';
+import { take } from 'rxjs';
+import { User } from 'src/app/models/user.model';
 
 @Component({
   selector: 'app-profile',
@@ -16,4 +20,28 @@ import { RouterModule } from '@angular/router';
   templateUrl: './profile-page.component.html',
   styleUrls: ['./profile-page.component.scss'],
 })
-export class ProfilePageComponent {}
+export class ProfilePageComponent implements OnInit {
+  constructor(
+    private userService: UserService,
+    private authService: AuthService
+  ) {}
+
+  id!: number;
+  user!: User;
+
+  ngOnInit(): void {
+    this.authService.user.pipe(take(1)).subscribe((user) => {
+      this.id = +user!.id;
+    });
+
+    if (this.id) {
+      this.userService
+        .getUserById(+this.id)
+        .pipe(take(1))
+        .subscribe((user) => {
+          console.log(user);
+          this.user = user;
+        });
+    }
+  }
+}
