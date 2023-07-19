22d11766be4884c4b1462a00046c35bb29e0e5c9
BerkayCan Ozcelik <brkycn10@gmail.com>
2023-07-19 14:57:14 +0200

    created profileDetailCard component

---
+++ src/app/components/profile/profile-detail-card/profile-detail-card.component.spec.ts
@@ -0,0 +1,21 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ProfileDetailCardComponent } from './profile-detail-card.component';
+
+describe('ProfileDetailCardComponent', () => {
+  let component: ProfileDetailCardComponent;
+  let fixture: ComponentFixture<ProfileDetailCardComponent>;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [ProfileDetailCardComponent]
+    });
+    fixture = TestBed.createComponent(ProfileDetailCardComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/components/profile/profile-detail-card/profile-detail-card.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-profile-detail-card',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './profile-detail-card.component.html',
+  styleUrls: ['./profile-detail-card.component.scss']
+})
+export class ProfileDetailCardComponent {
+
+}
