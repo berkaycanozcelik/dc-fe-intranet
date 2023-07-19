4e2de307b786a939b8d0ce5388f4b2bd93c4c919
BerkayCan Ozcelik <brkycn10@gmail.com>
2023-07-19 14:57:44 +0200

    adjusted profil page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,6 +5,8 @@ import { LandingComponent } from './pages/landing-page/landing-page.component';
 import { ProfileComponent } from './pages/profile-page/profile-page.component';
 import { HolidayComponent } from './pages/holiday-page/holiday-page.component';
 import { AuthGuard } from './services/auth/auth.guard';
+import { ProfileDetailCardComponent } from './components/profile/profile-detail-card/profile-detail-card.component';
+import { ProfileDetailUpdateCardComponent } from './components/profile/profile-detail-update-card/profile-detail-update-card.component';
 
 const routes: Routes = [
   {
@@ -14,7 +16,15 @@ const routes: Routes = [
     canActivate: [AuthGuard],
   }, // Default route
   { path: 'login', component: LoginComponent },
-  { path: 'profile', component: ProfileComponent, canActivate: [AuthGuard] },
+  {
+    path: 'profile',
+    component: ProfileComponent,
+    children: [
+      { path: '', component: ProfileDetailCardComponent },
+      { path: 'update', component: ProfileDetailUpdateCardComponent },
+    ],
+    canActivate: [AuthGuard],
+  },
   { path: 'holiday', component: HolidayComponent, canActivate: [AuthGuard] },
   { path: '**', redirectTo: '/' }, // Redirect to login page for any other route
 ];
--- src/app/pages/profile-page/profile-page.component.ts
+++ src/app/pages/profile-page/profile-page.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ProfileCardComponent } from 'src/app/components/profile/profile-card/profile-card.component';
+import { ProfileDetailCardComponent } from 'src/app/components/profile/profile-detail-card/profile-detail-card.component';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-profile',
   standalone: true,
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    ProfileCardComponent,
+    ProfileDetailCardComponent,
+    RouterModule,
+  ],
   templateUrl: './profile-page.component.html',
   styleUrls: ['./profile-page.component.scss'],
 })
