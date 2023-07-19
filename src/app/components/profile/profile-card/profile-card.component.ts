8d07efb313a404ea870d658c45a06bc18bc254fa
BerkayCan Ozcelik <brkycn10@gmail.com>
2023-07-19 17:00:02 +0200

    added user as Input to profile card

--- src/app/components/profile/profile-card/profile-card.component.ts
+++ src/app/components/profile/profile-card/profile-card.component.ts
@@ -1,13 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { User } from 'src/app/models/user.model';
 
 @Component({
   selector: 'app-profile-card',
   standalone: true,
   imports: [CommonModule],
   templateUrl: './profile-card.component.html',
-  styleUrls: ['./profile-card.component.scss']
+  styleUrls: ['./profile-card.component.scss'],
 })
 export class ProfileCardComponent {
-
+  @Input() user!: User;
+  
 }
