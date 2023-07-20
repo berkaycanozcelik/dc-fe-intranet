0471fd09b82321316a954b6b08ab1442d0fad612
BerkayCan Ozcelik <brkycn10@gmail.com>
2023-07-20 14:56:25 +0200

    added sorting to table

--- src/app/pages/management-page/management-page/management-page.component.ts
+++ src/app/pages/management-page/management-page/management-page.component.ts
@@ -4,32 +4,52 @@ import { UserService } from 'src/app/services/user/user.service';
 import { User } from 'src/app/models/user.model';
 import { MatTableDataSource, MatTableModule } from '@angular/material/table';
 import { MatPaginator, MatPaginatorModule } from '@angular/material/paginator';
+import { MatSort, MatSortModule, Sort } from '@angular/material/sort';
+import { LiveAnnouncer } from '@angular/cdk/a11y';
 
 @Component({
   selector: 'app-management-page',
   standalone: true,
-  imports: [CommonModule, MatTableModule, MatPaginatorModule],
+  imports: [CommonModule, MatTableModule, MatPaginatorModule, MatSortModule],
   templateUrl: './management-page.component.html',
   styleUrls: ['./management-page.component.scss'],
 })
 export class ManagementPageComponent implements OnInit {
   users: User[] = [];
 
-  displayedColumns: string[] = ['Name', 'LastName', 'Email', 'Role'];
+  displayedColumns: string[] = ['firstName', 'lastName', 'email', 'role'];
   dataSource = new MatTableDataSource<User>(this.users);
 
   @ViewChild(MatPaginator) paginator!: MatPaginator;
+  @ViewChild(MatSort) sort!: MatSort;
 
   ngAfterViewInit() {
     this.dataSource.paginator = this.paginator;
+    this.dataSource.sort = this.sort;
   }
 
-  constructor(private userService: UserService) {}
+  constructor(
+    private userService: UserService,
+    private _liveAnnouncer: LiveAnnouncer
+  ) {}
 
   ngOnInit(): void {
     this.userService.getAllUsers().subscribe((users) => {
       this.users = users;
       this.dataSource.data = this.users;
+      this.dataSource.sort = this.sort;
     });
+  }
+
+  announceSortChange(sortState: Sort) {
+    // This example uses English messages. If your application supports
+    // multiple language, you would internationalize these strings.
+    // Furthermore, you can customize the message to add additional
+    // details about the values being sorted.
+    if (sortState.direction) {
+      this._liveAnnouncer.announce(`Sorted ${sortState.direction}ending`);
+    } else {
+      this._liveAnnouncer.announce('Sorting cleared');
+    }
   }
 }
