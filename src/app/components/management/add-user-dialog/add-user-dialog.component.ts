cba87ceeff3b4067753415dda28e1ef8ab406680
BerkayCan Ozcelik <brkycn10@gmail.com>
2023-07-24 17:05:21 +0200

    created addUserDialog component

---
+++ src/app/components/management/add-user-dialog/add-user-dialog.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-add-user-dialog',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './add-user-dialog.component.html',
+  styleUrls: ['./add-user-dialog.component.scss']
+})
+export class AddUserDialogComponent {
+
+}
