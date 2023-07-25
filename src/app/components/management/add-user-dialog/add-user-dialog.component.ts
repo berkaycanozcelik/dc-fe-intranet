c9ff9f5f077d2982b80e93b1e64168503c19b9c9
BerkayCan Ozcelik <brkycn10@gmail.com>
2023-07-25 16:37:52 +0200

    added addUserDialog component

--- src/app/components/management/add-user-dialog/add-user-dialog.component.ts
+++ src/app/components/management/add-user-dialog/add-user-dialog.component.ts
@@ -1,13 +1,20 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Dialog } from '@angular/cdk/dialog';
+import { MatDialogModule, MatDialogRef } from '@angular/material/dialog';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-add-user-dialog',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, MatDialogModule, MatIconModule],
   templateUrl: './add-user-dialog.component.html',
-  styleUrls: ['./add-user-dialog.component.scss']
+  styleUrls: ['./add-user-dialog.component.scss'],
 })
 export class AddUserDialogComponent {
+  constructor(public dialogRef: MatDialogRef<AddUserDialogComponent>) {}
 
+  closeDialog() {
+    this.dialogRef.close();
+  }
 }
