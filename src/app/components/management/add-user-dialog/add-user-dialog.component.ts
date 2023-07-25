30d0586fb253fc90d30ca002a3c12ed6bd313c3d
BerkayCan Ozcelik <brkycn10@gmail.com>
2023-07-25 17:12:05 +0200

    adjsuted form template and added method call for register()

--- src/app/components/management/add-user-dialog/add-user-dialog.component.ts
+++ src/app/components/management/add-user-dialog/add-user-dialog.component.ts
@@ -3,16 +3,37 @@ import { CommonModule } from '@angular/common';
 import { Dialog } from '@angular/cdk/dialog';
 import { MatDialogModule, MatDialogRef } from '@angular/material/dialog';
 import { MatIconModule } from '@angular/material/icon';
+import { FormsModule, NgForm } from '@angular/forms';
+import { UserService } from 'src/app/services/user/user.service';
+import { AuthService } from 'src/app/services/auth/auth.service';
 
 @Component({
   selector: 'app-add-user-dialog',
   standalone: true,
-  imports: [CommonModule, MatDialogModule, MatIconModule],
+  imports: [CommonModule, MatDialogModule, MatIconModule, FormsModule],
   templateUrl: './add-user-dialog.component.html',
   styleUrls: ['./add-user-dialog.component.scss'],
 })
 export class AddUserDialogComponent {
-  constructor(public dialogRef: MatDialogRef<AddUserDialogComponent>) {}
+  constructor(
+    public dialogRef: MatDialogRef<AddUserDialogComponent>,
+    private authService: AuthService
+  ) {}
+
+  onSubmit(form: NgForm) {
+    console.log(form.value);
+    this.authService.register(form.value).subscribe(
+      (res) => {
+        this.dialogRef.close();
+        console.log(res);
+      },
+      (err) => {
+        console.log(err);
+      }
+    );
+
+    form.reset();
+  }
 
   closeDialog() {
     this.dialogRef.close();
