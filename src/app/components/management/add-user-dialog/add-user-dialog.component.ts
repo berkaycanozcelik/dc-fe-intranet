d89760deb0f979c7f07f807cc98f5a15a4978018
BerkayCan Ozcelik <brkycn10@gmail.com>
2023-07-26 12:56:30 +0200

    added update logic

--- src/app/components/management/add-user-dialog/add-user-dialog.component.ts
+++ src/app/components/management/add-user-dialog/add-user-dialog.component.ts
@@ -8,6 +8,7 @@ import {
 import { MatIconModule } from '@angular/material/icon';
 import { FormsModule, NgForm } from '@angular/forms';
 import { AuthService } from 'src/app/services/auth/auth.service';
+import { UserService } from 'src/app/services/user/user.service';
 
 @Component({
   selector: 'app-add-user-dialog',
@@ -20,7 +21,8 @@ export class AddUserDialogComponent {
   constructor(
     public dialogRef: MatDialogRef<AddUserDialogComponent>,
     @Inject(MAT_DIALOG_DATA) public data: any,
-    private authService: AuthService
+    private authService: AuthService,
+    private userService: UserService
   ) {}
 
   user = {
@@ -37,33 +39,52 @@ export class AddUserDialogComponent {
     team: '',
   };
 
+  isEditMode = false;
+
   ngOnInit() {
-    this.user = {
-      firstName: this.data.firstName,
-      lastName: this.data.lastName,
-      email: this.data.email,
-      password: this.data.password,
-      role: this.data.role,
-      address: this.data.address,
-      phoneNumber: this.data.phoneNumber,
-      birthday: this.data.birthday,
-      title: this.data.title,
-      manager: this.data.manager,
-      team: this.data.team,
-    };
+    if (this.data) {
+      this.user = {
+        firstName: this.data.firstName,
+        lastName: this.data.lastName,
+        email: this.data.email,
+        password: this.data.password,
+        role: this.data.role,
+        address: this.data.address,
+        phoneNumber: this.data.phoneNumber,
+        birthday: this.data.birthday,
+        title: this.data.title,
+        manager: this.data.manager,
+        team: this.data.team,
+      };
+      this.isEditMode = true;
+    }
   }
 
   onSubmit(form: NgForm) {
-    console.log(form.value);
-    this.authService.register(form.value).subscribe(
-      (res) => {
-        this.dialogRef.close();
-        console.log(res);
-      },
-      (err) => {
-        console.log(err);
-      }
-    );
+    if (!this.isEditMode) {
+      console.log(form.value);
+      this.authService.register(form.value).subscribe(
+        (res) => {
+          this.dialogRef.close();
+          console.log(res);
+        },
+        (err) => {
+          console.log(err);
+        }
+      );
+    } else {
+      console.log(form.value);
+      
+      this.userService.updateUser(this.data.id, form.value).subscribe(
+        (res) => {
+          this.dialogRef.close();
+          console.log('updated', res);
+        },
+        (err) => {
+          console.log('error by update', err);
+        }
+      );
+    }
 
     form.reset();
   }
